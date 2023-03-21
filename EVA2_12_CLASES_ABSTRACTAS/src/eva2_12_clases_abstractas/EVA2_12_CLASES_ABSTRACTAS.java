004dbc5af11c1321ab1715c3da7a13125cb571a4
RubenAcosta224 <124905283+RubenAcosta224@users.noreply.github.com>
2023-03-20 17:20:05 -0700

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //clases abstractas solo sirven para generalizar, pero no para crear objetos
+//solo para heredar, como superclases
+
+//metodos abstractos sirven con cabecera pero no implementacion
+//servirian solo para cuando hereden lo tengan y este obligado a sobreescribirlo
+
+
+//interfaces es un tipo de dato abstracto pero solo tiene metodos abstractos
+        
+        
+        
+        
+        //no se pueden crear objetos de clases abstractas
+        Prueba prueba=new Prueba();
+        PruebaAbstracta pabs=new PruebaAbstracta();
+    }
+}
+
+
+
+//
+//public class prueba{
+//    
+//}
+
+abstract class PruebaAbstracta{
+    
+}
